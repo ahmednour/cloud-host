071a47be2219642c339e54d35c8cf4f15863d27d
Ahmed Nour <darklevel152@gmail.com>
2024-04-16 14:20:44 +0300

    menu

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -4,7 +4,8 @@ import { GrTechnology } from "react-icons/gr";
 import styles from "./header.module.css";
 import { useState } from "react";
 import { LuMenu } from "react-icons/lu";
-import { TURBO_TRACE_DEFAULT_MEMORY_LIMIT } from "next/dist/shared/lib/constants";
+import { IoCloseOutline } from "react-icons/io5";
+
 
 const Navbar = () => {
     const [toogle, setToogle] = useState(false);
@@ -16,16 +17,16 @@ const Navbar = () => {
                     <GrTechnology />
                     Company
                 </Link>
-                <div>
-                    <LuMenu className={styles.icon} onClick={() => setToogle(prev => !prev)} />
+                <div className={styles.icon}>
+                    {toogle ? <IoCloseOutline onClick={() => setToogle(prev => !prev)} /> : <LuMenu onClick={() => setToogle(prev => !prev)} />}
                 </div>
             </div>
             <div className={styles.navLinksWraper} style={{ clipPath: toogle && "polygon(0 0, 100% 0, 100% 100%, 0 100%)" || "" }}>
                 <ul className={styles.navLinks}>
-                    <Link className={styles.navLink} href="/">Home</Link>
-                    <Link className={styles.navLink} href="/articles">Articles</Link>
-                    <Link className={styles.navLink} href="/about">About</Link>
-                    <Link className={styles.navLink} href="/admin">Admin Dashboard</Link>
+                    <Link className={styles.navLink} onClick={() => setToogle(false)} href="/">Home</Link>
+                    <Link className={styles.navLink} onClick={() => setToogle(false)} href="/articles">Articles</Link>
+                    <Link className={styles.navLink} onClick={() => setToogle(false)} href="/about">About</Link>
+                    <Link className={styles.navLink} onClick={() => setToogle(false)} href="/admin">Admin Dashboard</Link>
                 </ul>
             </div>
         </nav>
