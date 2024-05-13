12f95d3a8eac324001674e22b81e1add3da04bd0
Ahmed Nour <darklevel152@gmail.com>
2024-05-13 14:12:34 +0300

    add zod package

--- src/app/api/articles/route.js
+++ src/app/api/articles/route.js
@@ -1,4 +1,29 @@
 import { articles } from "@/utils/data";
+import { z } from "zod";
 export async function GET() {
   return Response.json(articles);
+}
+
+export async function POST(req) {
+  const data = await req.json();
+  // create validation to data input
+  const commentSchema = z.object({
+    title: z.string().min(2).max(200),
+    body: z.string().min(10),
+  });
+  const validation = commentSchema.safeParse(data);
+  if (!validation.success) {
+    return Response.json(
+      { message: validation.error.errors[0].message },
+      { status: 400 }
+    );
+  }
+  const newData = {
+    id: articles.length + 1,
+    userId: 200,
+    title: data.title,
+    body: data.body,
+  };
+  articles.push(newData);
+  return Response.json(newData, { status: 201 });
 }
